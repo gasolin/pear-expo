647f77f450c2b3c3b599b34573e33798dd6114a6
Fred <gasolin@gmail.com>
2025-01-17 11:01:21 +0800

    fix - update bare-kit to 0.4.6 and bare-pack script

--- app/script/bundle_worklet.sh
+++ app/script/bundle_worklet.sh
@@ -3,5 +3,35 @@
 echo "current bare-pack version:"
 npx bare-pack --version
 
+
+# Define the function
+barepack() {
+    if [ $# -ne 2 ]; then
+        echo "Usage: barepack <arg1> <arg2>"
+        return 1
+    fi
+
+    local arg1=$1
+    local arg2=$2
+    local targets=(
+        "ios-arm64"
+        "ios-arm64-simulator"
+        "ios-x64-simulator"
+        "android-arm"
+        "android-arm64"
+        "android-ia32"
+        "android-x64"
+    )
+
+    # Construct target flags
+    local target_flags=""
+    for target in "${targets[@]}"; do
+        target_flags+=" --target ${target}"
+    done
+
+    # Execute the command
+    npx bare-pack --out "${arg1}" "${arg2}" ${target_flags} --linked
+}
+
 # start compile bundles
-npx bare-pack --out worklet/app.bundle.js worklet/app.mjs
+barepack worklet/app.bundle.js worklet/app.mjs
